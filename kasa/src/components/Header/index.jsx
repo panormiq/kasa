e0b82d7732d763acdad52eac1a60d396edda5170
arnaud baratte <baratte.a@gmail.com>
2023-04-10 13:54:52 +0200

    css header

--- kasa/src/components/Header/index.jsx
+++ kasa/src/components/Header/index.jsx
@@ -3,13 +3,15 @@ import logo from '../../images/LOGO.png';
 import styles from './Header.module.css'; 
  
 function Header() {
+   
     return (
         <header className={styles.header}>
       
             <img src={logo} alt="Logo KASA"/>
             <nav className={styles.nav}>
-                <Link to="/">Accueil</Link>
-                <Link to="/about">A Propos</Link>
+             
+                <Link to="/" >Accueil</Link>
+                <Link to="/about"> A Propos</Link>
             </nav>
      
         </header>
