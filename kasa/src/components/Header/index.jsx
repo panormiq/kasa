952b13d966f9a8cc0d785033e85f223b1e6da377
arnaud baratte <baratte.a@gmail.com>
2023-04-10 11:24:42 +0200

    Ajout du logo dans le headers

--- kasa/src/components/Header/index.jsx
+++ kasa/src/components/Header/index.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom'
+import logo from '../../images/LOGO.png';
+import styles from './Header.module.css'; 
  
 function Header() {
     return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A Propos</Link>
-        </nav>
+        <header className={styles.header}>
+      
+            <img src={logo} alt="Logo KASA"/>
+            <nav className={styles.nav}>
+                <Link to="/">Accueil</Link>
+                <Link to="/about">A Propos</Link>
+            </nav>
+     
+        </header>
     )
 }
 
--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router ,Routes, Route } from "react-router-dom"
-import './index.css';
+import './style/css/index.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Logement from './pages/About';
