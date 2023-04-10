e82a7a2c9a7e7106a221f56c75a76f4f6002e489
arnaud baratte <baratte.a@gmail.com>
2023-04-11 00:04:34 +0200

    recuperation des donner json

---
+++ kasa/src/components/card/index.jsx
@@ -0,0 +1,9 @@
+ 
+function logementCard(logement) {
+    const logement1 = logement.scaleValue
+    return (
+       <div>{logement1.id}</div>
+    )
+}
+
+export default logementCard
--- kasa/src/pages/Home/index.jsx
+++ kasa/src/pages/Home/index.jsx
@@ -1,10 +1,23 @@
+import styles from './home.module.css';
+import loge
 function Home() {
   
+  const logements=require("../../data/logements.json")
+ 
     return (
-      <div>
-        <h1> Page d'accueil 🏠</h1>
-     
-      </div>
+      //a remplacer par un module image titre hauteur!!!!
+      <>
+        <div  className={styles.titre}>
+          <div  className={styles.img}>
+            <h1 className={styles.h1}> Chez vous, partout ailleurs</h1>
+          </div>
+        </div>
+        <div  className={styles.logement}>
+         {logements.map((logement) => ( <div>{logement.id}</div>))}
+
+        </div>
+      </>
+
     )
   }
   
